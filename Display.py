2fa15c65ca8cbb0e35f4d76be1cd141b19c42b06
deni336 <denison_carson@hotmail.com>
2021-12-23 23:59:20 -0600

    Strong Dashboard
    progress

--- Display.py
+++ Display.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 from tkinter.ttk import Entry
+import StrCalc as SC
 
 frameStyles = {"relief": "groove",
                "bd": 3, "bg": "#4b4b4b",
@@ -98,7 +99,21 @@ class CalculatorPage(GUI):
                           foreground="blue")
         label1.pack(side="top")
 
-        frame1 = tk.LabelFrame(self.mainFrame, frameStyles)
+        frame1 = tk.LabelFrame(self.mainFrame, frameStyles, text="Calculator Output")
+        frame1.place(rely=0.05, relx=0.58, height=800, width=800)
+        frame2 = tk.LabelFrame(self.mainFrame, frameStyles, text="Calculator Input")
+        frame2.place(rely=0.05, relx=0.02, height=400, width=600)
+        userNodes = Entry(frame2)
+        userNodes.insert(tk.END, "Number of Nodes")
+        userNodes.place(rely=0.05, relx=0.01)
+        daysToCalculate = Entry(frame2)
+        daysToCalculate.insert(tk.END, "Days to Calculate")
+        daysToCalculate.place(rely=0.10, relx=0.01)
+        writeBtn = ttk.Button(frame2, text="Submit",
+                                  command=lambda: SC.compute(userNodes, daysToCalculate))
+        writeBtn.place(rely=0.90, relx=0.70)
+        closeBtn = ttk.Button(frame2, text="Clear")
+        closeBtn.place(rely=0.90, relx=0.85)
 
         tv1 = ttk.Treeview(frame1)
         columnListAccount = ("Day", "Coin/day", "Total/coin", "Total Nodes", "Nodes Purchased")
@@ -117,8 +132,12 @@ class SettingsPage(GUI):
 
     def __init__(self, parent, controller):
 
-        GUI.__init__(self, parent) 
+        GUI.__init__(self, parent)
 
+        label1 = tk.Label(self.mainFrame, font=("Arial", 20),
+                        text="Settings", background="#4b4b4b",
+                        foreground="blue")
+        label1.pack(side="top")
 
 
 root = MyApp()
